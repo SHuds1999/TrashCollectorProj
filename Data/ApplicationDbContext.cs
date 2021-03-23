899241f9254691d87ff4a84be69257419c9bdc5e
SHuds1999 <52520173+SHuds1999@users.noreply.github.com>
2021-03-23 11:50:46 -0500

    Repo is up and ready to go again after deleting several times. Problem should be fixed. Added roll manager class via dependency injection.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TrashCollector.Models;
+
+namespace TrashCollector.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<Employee>Employees { get; set; }
+
+        public DbSet<Customer>Customers { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
